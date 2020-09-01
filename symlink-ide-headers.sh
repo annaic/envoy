f993f0e7a19d0d9c8a440d562576c4838b111ba3
annaic <rajan@annai.co>
2020-08-31 17:33:16 -0700

    Settings

---
+++ symlink-ide-headers.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+# This script symlinks bazel's output base into the root of the workspace
+# as a directory '/tmp/nighthawk_bazel_output_base'. This can be used by IDE's to
+# get to the header files of dependencies.
+# See .vscode/c_cpp_properties.json for an example.
+
+set -e
+
+if [ -f WORKSPACE ]; then
+    target=$(bazel info output_base)
+    ln -sf "$target/external/" external
+    echo "Symlinked $target to $(pwd)/external"
+else
+    echo "This script should be executed from the git root"
+    exit 1
+fi
